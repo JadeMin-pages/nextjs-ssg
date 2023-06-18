21717d3120e952a6225907967c76e0d8e7156202
JadeMin <alt0000@klartnet.ml>
2023-06-18 23:15:11 +0900

    style: edited

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -4,5 +4,5 @@ export const GET = async () => {
 	const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
 	const data = await response.json();
 
-	return NextResponse.json(data);
+	return NextResponse.json({data});
 }
